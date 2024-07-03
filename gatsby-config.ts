76422a177bf6e1a6207bfffbd39cce74162662bf
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-03 10:48:32 +0100

    .

---
+++ gatsby-config.ts
@@ -0,0 +1,34 @@
+import type { GatsbyConfig } from "gatsby"
+
+module.exports = {
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/assets/icon.png"
+      }
+    },
+
+    // ...
+  ],
+
+  pathPrefix: "",
+  
+
+}
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    siteUrl: `https://tobyhogan.space`,
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [],
+}
+
+export default config
+
+
