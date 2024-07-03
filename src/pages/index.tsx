89e5f212440e0f96037334a2b88f358216cf2df7
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-03 10:49:50 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ const mainStyles = {
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main className="">
-      <h1 className="text-5xl mx-auto w-fit bg-blue-500 mt-10 py-2 px-4">TSX. Gatsby. Tailwind.</h1>
+      <h1 className="text-4xl mx-auto w-fit mt-10 py-2 px-4">Metrics Page</h1>
     </main>
   )
 }
