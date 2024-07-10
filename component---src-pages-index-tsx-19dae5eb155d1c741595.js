6ab0a6a5bc104a3538290ec96158f47fdd09f896
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-10 13:51:23 +0100

    Updates

---
+++ component---src-pages-index-tsx-19dae5eb155d1c741595.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktobyhogan_space=self.webpackChunktobyhogan_space||[]).push([[245],{5823:function(e,t,a){a.r(t),a.d(t,{Head:function(){return c}});var n=a(758);t.default=()=>n.createElement("main",{className:""},n.createElement("h1",{className:"text-4xl underline mx-auto w-fit mt-10 py-2 px-4"},"Metrics Page"));const c=()=>n.createElement("title",null,"Home Page")}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-19dae5eb155d1c741595.js.map
